ac192d428aced12577254a91085cf12e268ad605
alanndz <alanmahmud0@gmail.com>
2020-12-15 10:02:38 +0000

    service.sh: pre_amount
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- service.sh
+++ service.sh
@@ -2,6 +2,7 @@
 # Written by Draco (tytydraco @ GitHub)
 
 # Setup tweaks
-nsenter -t 1 -m /system/bin/amount
-echo "amount: Running on boot"
+#nsenter -t 1 -m /system/bin/amount
+#echo "amount: Running on boot"
 #su -c "/system/bin/pre_amount"
+pre_amount
