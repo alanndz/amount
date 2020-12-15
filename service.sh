de1a3af852e34e884398aade39065faa9413147a
alanndz <alanmahmud0@gmail.com>
2020-12-15 10:04:40 +0000

    service.sh: test
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- service.sh
+++ service.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 # Written by Draco (tytydraco @ GitHub)
 
+echo "amount: running on boot" > /data/amount.log
+
 # Setup tweaks
 #nsenter -t 1 -m /system/bin/amount
 #echo "amount: Running on boot"
